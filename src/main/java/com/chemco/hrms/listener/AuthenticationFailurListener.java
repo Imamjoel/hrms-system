7197bc92e180cfa12c8eafdfdc3c73de683e9ff9
Imam Fahrizal <mfahreinheitz@gmail.com>
2020-08-03 20:52:16 +0700

    add event listener authentication

---
+++ src/main/java/com/chemco/hrms/listener/AuthenticationFailurListener.java
@@ -0,0 +1,30 @@
+package com.chemco.hrms.listener;
+
+import com.chemco.hrms.service.LoginAttemptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ExecutionException;
+
+@Component
+public class AuthenticationFailurListener {
+    private LoginAttemptService loginAttemptService;
+
+    @Autowired
+    public AuthenticationFailurListener(LoginAttemptService loginAttemptService) {
+        this.loginAttemptService = loginAttemptService;
+    }
+
+    @EventListener
+    public void onAuthenticationFailure(AuthenticationFailureBadCredentialsEvent event) {
+        Object principal = event.getAuthentication().getPrincipal();
+        if(principal instanceof String) {
+            String username = (String) event.getAuthentication().getPrincipal();
+            loginAttemptService.addUserToLoginAttemptCache(username);
+        }
+
+    }
+}
+
