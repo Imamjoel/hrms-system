05d4ff5ba3bda1825af8e36325b0bdf77e0f7f7f
Imam Fahrizal <mfahreinheitz@gmail.com>
2020-08-22 23:35:04 +0700

    -email hidden dulu

--- src/main/java/com/chemco/hrms/constant/EmailConstant.java
+++ src/main/java/com/chemco/hrms/constant/EmailConstant.java
@@ -3,8 +3,8 @@ package com.chemco.hrms.constant;
 public class EmailConstant {
     public static final String SIMPLE_MAIL_TRANSFER_PROTOCOL = "smtps";
     public static final String USERNAME = "mfahreinheitz@gmail.com";
-    public static final String PASSWORD = "Kmzw@y87aa";
-    public static final String FROM_EMAIL = "mfahreinheitz@gmail.com";
+    public static final String PASSWORD = "";
+    public static final String FROM_EMAIL = "";
     public static final String CC_EMAIL = "";
     public static final String EMAIL_SUBJECT = "PT. Chemco PM, HRMS - New Password";
     public static final String GMAIL_SMTP_SERVER = "smtp.gmail.com";
